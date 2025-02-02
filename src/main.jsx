855bededc658c3876b181c1749aa829880a23175
Mudasir Shah <mudasirkazis@gmail.com>
2025-02-02 10:21:53 +0500

    ssa

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppContextProvider>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </AppContextProvider>
   </StrictMode>
 );
